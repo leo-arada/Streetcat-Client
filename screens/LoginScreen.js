1697c55f3033bb49040570a442a1fe9b1b602b05
jiyong <arada3211@gmail.com>
2020-04-03 15:20:22 +0900

    Do authentication

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,17 +1,26 @@
 import React from 'react';
 import { StyleSheet, View, Text, Button } from 'react-native';
 import { CommonActions, StackActions  } from '@react-navigation/native';
+import * as Facebook from 'expo-facebook';
 
 const LogInScreen = ({ navigation }) => {
+
+  const login = async () => {
+    const ff = await Facebook.logInWithReadPermissionsAsync('<APP_ID>', { permission: ['public_profile'] });
+    console.log(ff);
+  };
+
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Button 
         title="google login"
-        onPress={() => 
+        onPress={() => {
+          login();
           navigation.dispatch(
             StackActions.replace('Home')
           )
         }
+        }     
       />
     </View>
   );
